bf246aa208257118d6562a4a24a5b21cf9c6b5cf
adutgg <setiadiadutt@gmail.com>
2024-08-28 02:39:07 +0700

    Add files via upload

---
+++ assets/config.js
@@ -0,0 +1,12 @@
+let config = {
+  "Title": "KETTEYOS - Links",
+  "Name": "KETTEYOS",
+  "Description": "Articel 1 PO NOW!",
+  "links": [
+    {
+      "Title": "WHATSAPP",
+      "URL": "http://wa.me/6285161479988",
+      "icon_classes": "fas fa-link",
+    },
+  ]
+}
